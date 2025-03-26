b9e409ccb4d18f843c0b47b99cf9f630f4a268bf
Denizbaranaykul <daykul75@gmail.com>
2025-03-26 23:31:59 +0300

    yeni

--- proje/Form1.Designer.cs
+++ proje/Form1.Designer.cs
@@ -33,7 +33,7 @@
             // 
             // button1
             // 
-            button1.Location = new Point(374, 203);
+            button1.Location = new Point(109, 119);
             button1.Name = "button1";
             button1.Size = new Size(94, 29);
             button1.TabIndex = 0;
