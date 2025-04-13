a91f9d09552aa607057fed0d2bab1dc0274d1fd1
Denizbaranaykul <daykul75@gmail.com>
2025-04-13 12:48:12 +0300

    dersleri ve notları okuyup yazdırma

--- proje/Obs.Designer.cs
+++ proje/Obs.Designer.cs
@@ -30,15 +30,15 @@
         {
             checkedListBox1 = new CheckedListBox();
             panel1 = new Panel();
+            panel2 = new Panel();
             lbl_bolum = new Label();
             btn_onay = new Button();
             label2 = new Label();
             label1 = new Label();
-            panel2 = new Panel();
             panel3 = new Panel();
-            button1 = new Button();
-            listBox1 = new ListBox();
             label3 = new Label();
+            listBox1 = new ListBox();
+            button1 = new Button();
             panel1.SuspendLayout();
             panel3.SuspendLayout();
             SuspendLayout();
@@ -46,43 +46,48 @@
             // checkedListBox1
             // 
             checkedListBox1.FormattingEnabled = true;
-            checkedListBox1.Location = new Point(38, 132);
-            checkedListBox1.Margin = new Padding(3, 2, 3, 2);
+            checkedListBox1.Location = new Point(43, 176);
             checkedListBox1.Name = "checkedListBox1";
-            checkedListBox1.Size = new Size(291, 220);
+            checkedListBox1.Size = new Size(332, 290);
             checkedListBox1.TabIndex = 1;
             // 
             // panel1
             // 
-            panel1.BackColor = Color.Gold;
+            panel1.BackColor = Color.LightBlue;
             panel1.Controls.Add(panel2);
             panel1.Controls.Add(lbl_bolum);
             panel1.Controls.Add(btn_onay);
             panel1.Controls.Add(label2);
             panel1.Controls.Add(label1);
             panel1.Controls.Add(checkedListBox1);
-            panel1.Location = new Point(-5, -2);
-            panel1.Margin = new Padding(3, 2, 3, 2);
+            panel1.Location = new Point(-6, -3);
             panel1.Name = "panel1";
-            panel1.Size = new Size(360, 550);
+            panel1.Size = new Size(411, 733);
             panel1.TabIndex = 2;
+            // 
+            // panel2
+            // 
+            panel2.Location = new Point(427, 4);
+            panel2.Margin = new Padding(3, 4, 3, 4);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(586, 729);
+            panel2.TabIndex = 3;
             // 
             // lbl_bolum
             // 
             lbl_bolum.BackColor = SystemColors.ButtonHighlight;
             lbl_bolum.Font = new Font("Segoe UI", 12F);
-            lbl_bolum.Location = new Point(38, 98);
+            lbl_bolum.Location = new Point(43, 131);
             lbl_bolum.Name = "lbl_bolum";
-            lbl_bolum.Size = new Size(291, 22);
+            lbl_bolum.Size = new Size(333, 29);
             lbl_bolum.TabIndex = 6;
             lbl_bolum.Text = "label3";
             // 
             // btn_onay
             // 
-            btn_onay.Location = new Point(78, 414);
-            btn_onay.Margin = new Padding(3, 2, 3, 2);
+            btn_onay.Location = new Point(89, 552);
             btn_onay.Name = "btn_onay";
-            btn_onay.Size = new Size(225, 68);
+            btn_onay.Size = new Size(257, 91);
             btn_onay.TabIndex = 4;
             btn_onay.Text = "onayla";
             btn_onay.UseVisualStyleBackColor = true;
@@ -91,75 +96,72 @@
             // label2
             // 
             label2.AutoSize = true;
-            label2.Location = new Point(38, 66);
+            label2.Location = new Point(43, 88);
             label2.Name = "label2";
-            label2.Size = new Size(105, 15);
+            label2.Size = new Size(128, 20);
             label2.TabIndex = 3;
             label2.Text = "bölümünüzü seçin";
             // 
             // label1
             // 
             label1.Font = new Font("Segoe UI", 16F);
-            label1.Location = new Point(78, 18);
+            label1.Location = new Point(89, 24);
             label1.Name = "label1";
-            label1.Size = new Size(225, 30);
+            label1.Size = new Size(257, 40);
             label1.TabIndex = 2;
             label1.Text = " Ders kayıt işlemleri";
             // 
-            // panel2
-            // 
-            panel2.Location = new Point(374, 3);
-            panel2.Name = "panel2";
-            panel2.Size = new Size(513, 547);
-            panel2.TabIndex = 3;
-            // 
             // panel3
             // 
-            panel3.BackColor = Color.LimeGreen;
+            panel3.BackColor = Color.SteelBlue;
             panel3.Controls.Add(label3);
             panel3.Controls.Add(listBox1);
             panel3.Controls.Add(button1);
-            panel3.Location = new Point(361, -2);
+            panel3.Location = new Point(411, 1);
+            panel3.Margin = new Padding(3, 4, 3, 4);
             panel3.Name = "panel3";
-            panel3.Size = new Size(390, 557);
+            panel3.Size = new Size(657, 743);
             panel3.TabIndex = 3;
-            // 
-            // button1
-            // 
-            button1.Location = new Point(112, 414);
-            button1.Name = "button1";
-            button1.Size = new Size(176, 68);
-            button1.TabIndex = 0;
-            button1.Text = "Yenile";
-            button1.UseVisualStyleBackColor = true;
-            // 
-            // listBox1
-            // 
-            listBox1.FormattingEnabled = true;
-            listBox1.ItemHeight = 15;
-            listBox1.Location = new Point(32, 93);
-            listBox1.Name = "listBox1";
-            listBox1.Size = new Size(324, 304);
-            listBox1.TabIndex = 1;
             // 
             // label3
             // 
             label3.Font = new Font("Segoe UI", 16F);
-            label3.Location = new Point(32, 18);
+            label3.Location = new Point(179, 24);
             label3.Name = "label3";
-            label3.Size = new Size(324, 30);
+            label3.Size = new Size(255, 40);
             label3.TabIndex = 3;
             label3.Text = "Ders Notları";
             label3.TextAlign = ContentAlignment.MiddleCenter;
             // 
+            // listBox1
+            // 
+            listBox1.Font = new Font("Segoe UI", 18F);
+            listBox1.FormattingEnabled = true;
+            listBox1.ItemHeight = 41;
+            listBox1.Location = new Point(37, 124);
+            listBox1.Margin = new Padding(3, 4, 3, 4);
+            listBox1.Name = "listBox1";
+            listBox1.Size = new Size(538, 373);
+            listBox1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(179, 552);
+            button1.Margin = new Padding(3, 4, 3, 4);
+            button1.Name = "button1";
+            button1.Size = new Size(255, 91);
+            button1.TabIndex = 0;
+            button1.Text = "Yenile";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
             // Obs
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(1392, 546);
+            ClientSize = new Size(1591, 728);
             Controls.Add(panel3);
             Controls.Add(panel1);
-            Margin = new Padding(3, 2, 3, 2);
             Name = "Obs";
             Text = "Obs";
             panel1.ResumeLayout(false);
--- proje/Obs.cs
+++ proje/Obs.cs
@@ -17,28 +17,69 @@ namespace proje
     {
         public List<string> secilen = new List<string>();
 
+        public static List<string> derslers = new List<string>();
+
+        public List<string> derslersecilen = new List<string>();
+
         public string[] dersIsletme = { "İşletme", "Maliye", "Pazarlama", "Ekonomi", "Exel", "Tarih", "İngilizce", "Matematik" };
         public string[] dersYbs = { "İşletme", "Maliye", "Pazarlama", "Hukuk", "Programlama", "Tarih", "İngilizce", "Matematik" };
         public string[] dersBilgisayar = { "İşletme", "Mühendisliğe giriş", "Kimya", "Fizik", "Programlama", "Tarih", "İngilizce", "Matematik" };
 
+        public static void Okuma(List<string> a, string query, string get, int x)
+        {
+            GlobalDatabase.Conn.Open();
 
+            MySqlCommand komut = new MySqlCommand(query, GlobalDatabase.Conn);
+            using (MySqlDataReader reader = komut.ExecuteReader())
+            {
+                while (reader.Read())
+                {
+                    if (x == 1)
+                    {
+                        string ders = reader.GetString(get);
+                        a.Add(ders);
+                    }
+                    else
+                    {
+                        int vize = reader["VizeNotu"] == DBNull.Value ? 0 : Convert.ToInt32(reader["VizeNotu"]);
+                        int final = reader["FinalNotu"] == DBNull.Value ? 0 : Convert.ToInt32(reader["FinalNotu"]);
+                        string satir = $"{reader["dersAdi"]} - Vize: {vize} Final: {final}";
+                        a.Add(satir);
+                    }
+                }
+            }
+            GlobalDatabase.Conn.Close();
+        }
         public Obs()
         {
             InitializeComponent();
-            if (Giris.taban==0)
+            if (Giris.taban == 0)
             {
 
-            
-            lbl_bolum.Text = GlobalDatabase.Dt.Rows[0]["bolum"].ToString();
-        }
+
+                lbl_bolum.Text = GlobalDatabase.Dt.Rows[0]["bolum"].ToString();
+                string query = "SELECT ders FROM dersler";
+                Okuma(derslers, query, "ders", 1);
+                string query2 = "SELECT dersAdi,VizeNotu,FinalNotu FROM ders_notlari";
+                Okuma(derslersecilen, query2, "dersAdi", 2);
+
+
+            }
             switch (lbl_bolum.Text)
             {
                 case "İşletme":
                     checkedListBox1.Items.AddRange(dersIsletme);
                     break;
-                    
+
                 case "Yönetim Bilişim Sistemleri":
-                    checkedListBox1.Items.AddRange(dersYbs);
+                    foreach (string item in derslers)
+                    {
+                        checkedListBox1.Items.Add(item);
+                    }
+                    foreach (string item in derslersecilen)
+                    {
+                        listBox1.Items.Add(item);
+                    }
                     break;
 
                 case "Bilgisayar Mühendisliği":
@@ -56,30 +97,70 @@ namespace proje
         {
             secilen.Clear();
 
-            
-            
+
+
             try
             {
-            GlobalDatabase.Conn.Open();
-            string sorgu = "INSERT INTO ders_notlari (OgrenciId,DersAdi) VALUES(@OgrenciId,@DersAdi)";
-            MySqlCommand mySqlCommand = new MySqlCommand(sorgu,GlobalDatabase.Conn);
+                GlobalDatabase.Conn.Open();
+                string sorgu = "INSERT INTO ders_notlari (OgrenciId,DersAdi) VALUES(@OgrenciId,@DersAdi)";
+                MySqlCommand mySqlCommand = new MySqlCommand(sorgu, GlobalDatabase.Conn);
                 int ogrenciId = Convert.ToInt32(GlobalDatabase.Dt.Rows[0]["id"]);
-            foreach (var item in checkedListBox1.CheckedItems)
-            {
-                
-                    mySqlCommand.Parameters.Clear();
-                    mySqlCommand.Parameters.AddWithValue("@OgrenciId", ogrenciId);   
-                    mySqlCommand.Parameters.AddWithValue("@DersAdi", item.ToString());
-                    mySqlCommand.ExecuteNonQuery();//sorgu çalıştırma
-            }
-                MessageBox.Show("Seçilen dersler başarıyla eklendi.");
+                int kredi = 0;
+                string insertSorgu = "INSERT INTO ders_notlari (OgrenciId, DersAdi) VALUES(@OgrenciId, @DersAdi)";
+                MySqlCommand insertCommand = new MySqlCommand(insertSorgu, GlobalDatabase.Conn);
+                foreach (var item in checkedListBox1.CheckedItems)
+                {
+                    string dersAdi = item.ToString();
+
+                    string krediSorgu = "SELECT kredi FROM dersler WHERE ders = @dersAdi";
+                    MySqlCommand krediCmd = new MySqlCommand(krediSorgu, GlobalDatabase.Conn);
+                    krediCmd.Parameters.AddWithValue("@dersAdi", dersAdi);
+
+                    object sonuc = krediCmd.ExecuteScalar();
+                    if (sonuc != null)
+                    {
+                        int kredi_gecici = Convert.ToInt32(sonuc);
+                        kredi += kredi_gecici;
+                    }
+                }
+
+
+
+                if (kredi <= 30)
+                {
+                    foreach (var item in checkedListBox1.CheckedItems)
+                    {
+
+                        mySqlCommand.Parameters.Clear();
+                        mySqlCommand.Parameters.AddWithValue("@OgrenciId", ogrenciId);
+                        mySqlCommand.Parameters.AddWithValue("@DersAdi", item.ToString());
+
+                        mySqlCommand.ExecuteNonQuery();//sorgu çalıştırma
+                    }
+                    MessageBox.Show("Seçilen dersler başarıyla eklendi.");
+                }
+                else
+                {
+                    MessageBox.Show("Toplam kredi 30'u geçemez! Seçilen krediler: " + kredi);
+                }
+
+
+
             }
             catch (Exception ex)
             {
-               MessageBox.Show("Ders ismi eklenirken bir hata oluştu : "+ex.Message);
+                MessageBox.Show("Ders ismi eklenirken bir hata oluştu : " + ex.Message);
+            }
+            finally
+            {
+                GlobalDatabase.Conn.Close();
             }
         }
 
-        
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string query2 = "SELECT dersAdi,VizeNotu,FinalNotu FROM ders_notlari";
+            Okuma(derslersecilen, query2, "dersAdi", 2);
+        }
     }
 }
